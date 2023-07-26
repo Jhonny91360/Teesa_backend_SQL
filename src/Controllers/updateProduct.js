104f0a9f5e194aaaec7ff356d17ac16bd92bf875
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-07-26 10:29:11 -0500

    se modifica public id agregando la carpeta products a la ruta

--- src/Controllers/updateProduct.js
+++ src/Controllers/updateProduct.js
@@ -19,11 +19,13 @@ const updateProduct=async(req,res)=>{
         if(imagenes.length>0){
             //Elimino las imagenes que tenia en clodinary de ese producto
             if(product	){
-                console.log("Imagenes ya guardadas ",imagenes);
+                
                 for (const imagen of product.imagenes) {
                     console.log("Imagen a eliminar: ",imagen)
-                    const publicId = await cloudinary.url(imagen, { type: 'upload' }).split('/').pop().split('.')[0];
+                    let publicId = await cloudinary.url(imagen, { type: 'upload' }).split('/').pop().split('.')[0];
                     console.log("public ID obtenido: ",publicId)
+                    publicId=`products/${publicId}`
+                    console.log("public ID modificado: ",publicId)
                     const clodinaryDelete= await cloudinary.uploader.destroy(publicId)
                 }
             }
