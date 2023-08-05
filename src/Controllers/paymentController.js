e4ef88c3cf89d5d93f9b10fb46acabed81ef4f58
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-08-05 12:26:21 -0500

    se agrega token de teesa para pagos reales

--- src/Controllers/paymentController.js
+++ src/Controllers/paymentController.js
@@ -8,7 +8,9 @@ const createOrder = async (req,res)=>{
 
     mercadopago.configure({
         //access_token: "TEST-506669402188981-061413-d4d1537702f9ee8b568253de5e4b879f-1399136636"
-        access_token:"TEST-6291707727318397-080115-94a23194e5ec1601243fd310cf7dd056-1439388864"
+        //access_token:"TEST-6291707727318397-080115-94a23194e5ec1601243fd310cf7dd056-1439388864"
+        //toke teesa:APP_USR-5406266318410811-080416-fe0c71c68815bfeebeadb435586f510f-1281028042
+        access_token:"APP_USR-5406266318410811-080416-fe0c71c68815bfeebeadb435586f510f-1281028042"
     })
 
     // MANDAR POR GOOGLE ID
